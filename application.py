fded1a9e2aec590b4f5205a8ba11d2194e5d5388
Haneesh H <Haneesh.H@gds.ey.com>
2020-12-15 23:35:12 +0530

    modin test

--- application.py
+++ application.py
@@ -19,10 +19,18 @@ def modinpd():
     #q=request.args['q']  
     start = timeit.default_timer()
     df = pd.read_csv("GL_Data.csv")
-    count=len(df)
     stop = timeit.default_timer()
+
+    count=len(df)
+    
     print('Time modin pd taken: ', stop - start) 
     #output = json.dumps(result, default=set_default)
+    s = time.time()
+    df = pd.concat([df for _ in range(5)])
+    e = time.time()
+    print("Pandas Concat Time = {}".format(e-s))
+
+
     return str(count)
 
 @app.route('/normal-pd')
@@ -30,10 +38,48 @@ def normalpd():
     #q=request.args['q'] 
     start = timeit.default_timer() 
     dfn = pdn.read_csv("GL_Data.csv")
-    count=len(dfn)
     stop = timeit.default_timer()
+
+    count=len(dfn)
+    
     print('Time normal pd taken: ', stop - start) 
     #output = json.dumps(result, default=set_default)
+    return str(count)
+
+@app.route('/test')
+def normalpd():
+    #q=request.args['q'] 
+
+    ### Read in the data with Pandas   
+
+    s = time.time()
+    df = pdn.read_csv("GL_Data.csv")
+    e = time.time()
+    print("Pandas Loading Time = {}".format(e-s))
+
+    ### Read in the data with Modin    
+
+    s = time.time()
+    df = pd.read_csv("GL_Data.csv")
+    e = time.time()
+    print("Modin Loading Time = {}".format(e-s))
+
+    df = pdn.read_csv("GL_Data.csv")
+    s = time.time()
+    df = pdn.concat([df for _ in range(5)])
+    e = time.time()
+    print("Pandas Concat Time = {}".format(e-s))
+
+
+    df = pd.read_csv("GL_Data.csv")
+    s = time.time()
+    df = pd.concat([df for _ in range(5)])
+    e = time.time()
+    print("Modin Concat Time = {}".format(e-s))
+
+
+
+    count=len(df)
     return str(count)
 
 
